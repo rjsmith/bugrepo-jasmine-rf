28174ab20a26c8bc02c0bc45045adce5babad20f
Richard Smith <rsmith@rsbatechnology.co.uk>
2015-03-12 21:29:21 +0000

    cucumber then jasmine throws error
    Order of adding packages:
    
    `meteor add xolvio:cucumber`
    `meteor add sanjo:jasmine`

--- packages/tests-proxy/package.js
+++ packages/tests-proxy/package.js
@@ -11,9 +11,4 @@ Package.onUse(function (api) {
 	api.add_files("tests/jasmine/server/integration/sample/spec/SpecMatchers.js",["server"]);
 	api.add_files("tests/jasmine/server/integration/sample/src/Player.js",["server"]);
 	api.add_files("tests/jasmine/server/integration/sample/src/Song.js",["server"]);
-	api.add_files("tests/robotframework/arguments.txt",["server","client"]);
-	api.add_files("tests/robotframework/suites/resources.txt",["server","client"]);
-	api.add_files("tests/robotframework/suites/test-html.html",["server","client"]);
-	api.add_files("tests/robotframework/suites/test-tsv.tsv",["server","client"]);
-	api.add_files("tests/robotframework/suites/test-txt.txt",["server","client"]);
 });
